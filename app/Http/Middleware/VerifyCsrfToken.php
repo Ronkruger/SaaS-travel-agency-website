44d5de419378033fa8353c811d5437b552c0ecc4
DiscoverGroup <romanolantano.discovergrp@gmail.com>
2026-04-21 10:18:04 +0800

    Fix tenant admin logout 419 by exempting logout from CSRF

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -9,5 +9,8 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'xendit/webhook',
         'stripe/webhook',
+        // Allow logout even when session/CSRF token has expired.
+        'logout',
+        '*/logout',
     ];
 }
